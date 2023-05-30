a320e4c5701e66d0f2e848646f147697c01c52dc
LeJammes454 <98853831+LeJammes454@users.noreply.github.com>
2023-05-30 00:32:12 -0600

    consumo de api

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,27 +1,65 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import { Haptics } from '@capacitor/haptics';
-import React from 'react';
-import './Tab4.css';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonGrid, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import axios from 'axios';
+import { Network, NetworkStatus } from '@capacitor/network';
+import React, { useEffect, useState } from 'react';
 
 const Tab4: React.FC = () => {
-  const playHapticFeedback = () => {
-    Haptics.vibrate();
-  };
+  const [characters, setCharacters] = useState([]);
+  const [networkStatus, setNetworkStatus] = useState<NetworkStatus | null>(null);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await axios.get('https://rickandmortyapi.com/api/character');
+        setCharacters(response.data.results);
+      } catch (error) {
+        console.log('Error al obtener los personajes:', error);
+      }
+    };
+
+    fetchCharacters();
+
+    const networkListener = Network.addListener('networkStatusChange', (status: NetworkStatus) => {
+      setNetworkStatus(status);
+    });
+
+    return () => {
+      networkListener.remove();
+    };
+  }, []);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Geolocalización</IonTitle>
+          <IonTitle>Lista de Personajes</IonTitle>
         </IonToolbar>
+        {networkStatus && (
+          <IonToolbar>
+            <IonTitle>
+              {networkStatus.connected ? 'Conectado a Internet' : 'Sin conexión'}
+              <br />
+              Tipo de conexión: {networkStatus.connectionType}
+            </IonTitle>
+          </IonToolbar>
+        )}
       </IonHeader>
-      <IonContent fullscreen className="ion-padding">
-        <div className="haptic-feedback-container">
-          <h2>Retroalimentación Háptica</h2>
-          <IonButton expand="full" onClick={playHapticFeedback}>
-            Activar retroalimentación háptica
-          </IonButton>
-        </div>
+      <IonContent>
+        <IonGrid>
+          {characters.map((character) => (
+            <IonCard key={character.id} size="12" sizeXs="6" sizeSm="4" sizeMd="3">
+              <img src={character.image} alt={character.name} />
+              <IonCardHeader>
+                <IonCardTitle>{character.name}</IonCardTitle>
+                <IonCardSubtitle>{character.species}</IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <p>Status: {character.status}</p>
+                <p>Location: {character.location.name}</p>
+              </IonCardContent>
+            </IonCard>
+          ))}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
